76d8e92d9731d1242f7e8c8e0a99f8485185fba2
ignacio <nachomonllorc@gmail.com>
2020-02-27 04:24:44 -0300

    agrego archivos

---
+++ Maximum Multiple.cs
@@ -0,0 +1,7 @@
+﻿ 
+https://www.codewars.com/kata/5aba780a6a176b029800041c/solutions/csharp
+
+public static int MaxMultiply(int divisor, int bound)
+    {
+        return bound / divisor * divisor;
+    }
